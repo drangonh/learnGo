35dc83e82339b18176b73102f2d88d6557e95d64
hl <786676631@qq.com>
2020-02-24 16:58:11 +0800

    学习http服务的测试

--- nonrepeatingsubstr/nonrepeating.go
+++ nonrepeatingsubstr/nonrepeating.go
@@ -1,14 +1,12 @@
 package main
 
-import "fmt"
-
-var lastOccurred = make([]int, 0xffff) //初始了65535字节的大小
+//var lastOccurred = make([]int, 0xffff) //初始了65535字节的大小
 
 func lengthOfNonRepeatingSubStr(s string) int {
 
 	//这里用slice来优化map，利用空间换时间，达到优化运行速度的效果
 	//lastOccurred := make(map[rune]int)
-	//lastOccurred := make([]int, 0xffff)
+	lastOccurred := make([]int, 0xffff)
 
 	for i := range lastOccurred {
 		lastOccurred[i] = -1
@@ -18,8 +16,11 @@ func lengthOfNonRepeatingSubStr(s string) int {
 	num := 0
 
 	for i, ch := range []rune(s) {
-		fmt.Println(i, ch)
-		if lastI := lastOccurred[ch]; lastOccurred[ch] != -1 && lastI >= start {
+		//if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+		//	start = lastI + 1
+		//}
+
+		if lastI := lastOccurred[ch]; lastI != -1 && lastI >= start {
 			start = lastI + 1
 		}
 
